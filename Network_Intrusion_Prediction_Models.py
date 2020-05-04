daa41aa5d778e59019fcb4c453f1ec12c0cc943d
Tyler Lane Howard <tug65404@temple.edu>
2020-05-03 23:30:25 -0400

    initial commit

---
+++ Network_Intrusion_Prediction_Models.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from scipy.io.arff import loadarff
+from sklearn.naive_bayes import GaussianNB
+import sklearn_NaiveBayees_UNSW_NB15
+
+# 0 will be UNSW
+datasets = ["unsw", "nsl-kdd"]
+algorithms = []
+
+def main():
+    dataset_selection = get_dataset_selection()
+    algorithm_selection = get_algorithm_selection()
+
+    train, test = load_nb15_csv()
+    sklearn_NaiveBayees_UNSW_NB15.run_naiveBayes(train, test)
+
+def get_dataset_selection():
+    while True:
+        print("Which dataset would you like to use:")
+        # TODO surround with for loop to print each thing listed in dataset
+        print("1. UNSW-NB15 (csv)")
+        print("2. NSL-KDD (arff)")
+
+        try:
+            user_input = int(input("Your selection: "))
+            if user_input > len(datasets) or user_input < 1:
+                raise ValueError
+        except ValueError:
+            print("\nPlease enter a valid number\n\n")
+            continue
+        else:
+            return user_input
+
+
+def get_algorithm_selection():
+    print()
+
+
+def load_nb15_csv():
+    nb15_train = pd.read_csv('Datasets/UNSW-NB15/csv/UNSW_NB15_training-set.csv', delimiter=',')
+    nb15_test = pd.read_csv('Datasets/UNSW-NB15/csv/UNSW_NB15_testing-set.csv', delimiter=',')
+
+    return nb15_train, nb15_test
+
+
+def load_nslkdd_arff():
+    kdd_train, train_metadata = loadarff("KDDTrain+.arff")
+    kdd_test, test_metadata = loadarff("KDDTest+.arff")
+
+    return kdd_train, kdd_test
+
+
+if __name__ == "__main__":
+    main()
