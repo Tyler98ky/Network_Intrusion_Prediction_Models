689d174ebf6e0c9659f829f4792f55341c9e16ae
Tyler Lane Howard <tug65404@temple.edu>
2020-05-04 03:58:33 -0400

    added confusion matrix output to naive bayes

--- sklearn_NaiveBayees.py
+++ sklearn_NaiveBayees.py
@@ -1,21 +1,18 @@
 from sklearn.naive_bayes import GaussianNB
-
+import sklearn as sk
 
 def run_naive_bayes(X_train, X_test, y_train, y_test):
-
     # Fit to model and predict
     gnb = GaussianNB()
     y_pred = gnb.fit(X_train, y_train).predict(X_test)
 
-    total_datapoints = X_test.shape[0]
-    mislabeled_datapoints = (y_test != y_pred).sum()
-    correct_datapoints = total_datapoints - mislabeled_datapoints
-    percent_correct = (correct_datapoints / total_datapoints) * 100
+    positve_label = "anomaly"
+    negative_label = "normal"
+    padding = " " * max(len(positve_label), len(negative_label))
 
-    print("NaiveBayes results:\n")
-    print("Total datapoints: %d\nCorrect datapoints: %d\nMislabeled datapoints: %d\nPercent correct: %.2f%%"
-          % (total_datapoints, correct_datapoints, mislabeled_datapoints, percent_correct))
-
-
-
-
+    print("")
+    print("{}{} {}".format(padding, positve_label, negative_label))
+    confusion_matrix = sk.metrics.confusion_matrix(y_test, y_pred, labels=[0, 1])
+    print("{} {}".format(positve_label, confusion_matrix[0]))
+    print("{} {}".format(negative_label, confusion_matrix[1]))
+    print("Percent correct: {:.3%}".format(sk.metrics.accuracy_score(y_test, y_pred)))
