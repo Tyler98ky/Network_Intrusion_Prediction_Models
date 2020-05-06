68938e5a7af9f6ed6cce91a23412948734b35add
Tyler Lane Howard <tug65404@temple.edu>
2020-05-06 13:08:26 -0400

    removed functionality to automatically display gui confusion matrix

--- sklearn_NaiveBayees.py
+++ sklearn_NaiveBayees.py
@@ -9,5 +9,5 @@ def run_naive_bayes(X_train, X_test, y_train, y_test):
     y_pred = gnb.fit(X_train, y_train).predict(X_test)
 
     gnb_disp = plot_confusion_matrix(gnb, X_test, y_test)
-    plt.show()
+    # plt.show()
     return y_pred
