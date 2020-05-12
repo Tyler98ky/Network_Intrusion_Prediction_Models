88c650b0eac31d449946f7832cfd3e12d08b9ac3
Tyler Howard <tyler98ky@gmail.com>
2020-05-12 18:29:08 -0400

    removed matplotlib call because of segfault issues on MacOS

--- sklearn_NaiveBayees.py
+++ sklearn_NaiveBayees.py
@@ -8,6 +8,6 @@ def run_naive_bayes(X_train, X_test, y_train, y_test):
     gnb = GaussianNB()
     y_pred = gnb.fit(X_train, y_train).predict(X_test)
 
-    gnb_disp = plot_confusion_matrix(gnb, X_test, y_test)
+    # gnb_disp = plot_confusion_matrix(gnb, X_test, y_test)
     # plt.show()
     return y_pred
